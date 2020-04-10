2598ece23003990cf151569dc5635fcbee296b0f
Ha Nguyen <nguyenthingocha2@gmail.com>
2020-04-10 10:12:06 -0400

    Updates

---
+++ precache-manifest.dd478e42ba5fa2526e6181f2e90a8097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555b87bfebd5dbec530a0ba82eafec43",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "885197e3f169a7fedb04",
+    "url": "/my-react-portfolio/static/css/main.dea4fbd6.chunk.css"
+  },
+  {
+    "revision": "16d15a81aea023b1df73",
+    "url": "/my-react-portfolio/static/js/2.a38848cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-react-portfolio/static/js/2.a38848cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885197e3f169a7fedb04",
+    "url": "/my-react-portfolio/static/js/main.0d578210.chunk.js"
+  },
+  {
+    "revision": "0c530593fa9a17e82b1e",
+    "url": "/my-react-portfolio/static/js/runtime-main.dee2495f.js"
+  }
+]);
